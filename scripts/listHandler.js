2d83fffb1da3acbf6b17e8d8a48f6ed09cca0bdf
karthikeya.kona <kartkona21@ncs.charter.k12.de.us>
2019-11-01 14:23:34 -0400

    changes

---
+++ scripts/listHandler.js
@@ -0,0 +1,9 @@
+// const list = require('./list');
+// const cheerio = require('cheerio');
+// const $ = cheerio.load(fs.readFileSync('./ListTemplate.html'));
+//
+//
+// $('#id').add()
+// function generateListHtml() {
+//
+// }
