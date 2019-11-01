34a3e5723e1dc33ba902bbb2c7461ac92a57e7e7
greenpizza12 <greenpizza12@users.noreply.github.com>
2019-10-31 21:08:37 -0400

    made changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 // var getDirName = require('path').dirname;
 // var express = require('express');
-var floomaticHanlder = require('./floomaticHandler');
 // var request = require('request');
 // var fse = require('fs-extra');
-floomaticHanlder.init();
 const expressHandler = require('./expressHandler');
 expressHandler.init();
 
