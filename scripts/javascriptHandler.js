a28884437dd0f6fc1b4352d4b4db11d1d87da2c4
greenpizza1203 <50718974+greenpizza1203@users.noreply.github.com>
2019-11-04 09:14:21 -0500

    Update javascriptHandler.js

--- scripts/javascriptHandler.js
+++ scripts/javascriptHandler.js
@@ -7,7 +7,7 @@ function checkDomain(filePath) {
     replace.sync({
         files: filePath,
         from: /coolmathgames/g,
-        to: 'herokuapp',
+        to: 'math-stuff.herokuapp',
     });
 
 }
